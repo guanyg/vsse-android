6451c0a79b7299c74a0cc2622d689f7f229a27fd
guanyg <guan_yg@163.com>
2018-03-19 16:13:17 +0800

    Modify messages in the MainActivity

--- app/src/main/java/vsse/vsse_and/MainActivity.java
+++ app/src/main/java/vsse/vsse_and/MainActivity.java
@@ -1,5 +1,7 @@
 package vsse.vsse_and;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -16,8 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ClipboardManager clipboardManager = (ClipboardManager) getApplicationContext().getSystemService(CLIPBOARD_SERVICE);
+        if(!clipboardManager.hasPrimaryClip())
+            return;
+        ClipData c = clipboardManager.getPrimaryClip();
+        String s = c.getItemAt(0).getText().toString();
+        if(!s.startsWith("vsse://"))
+            return;
+
         Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-        intent.setData(Uri.parse("vsse://10.21.238.12:5678/?k=swtnylqmrbocgixjahfzpudkev&k0=AMnnACF5vH7RxyrB&k1=LZ5IBWVWHzXe3c8a"));
+        intent.setData(Uri.parse(s));
         startActivity(intent);
         finish();
     }
